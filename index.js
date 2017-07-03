875e0a888a30f3a15ac8842187f58ba32fee5a1d
Taye Adeyemi <dev@taye.me>
2017-07-03 23:04:14 +0200

    Update more things

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const $ = require('queryselectorall');
+var $ = require('queryselectorall');
+
+var defaults = {
+  HTMLSelector: 'code.language-xml,code.language-html,code.language-markup,code.xml,code.html',
+  CSSSelector : 'code.language-css,code.css',
+  JSSelector  : 'code.language-javascript,code.javascript',
+
+  HTMLFlag: /^<!--.*enable javascript.*-->/,
+  CSSFlag : /^\/\*.*enable javascript.*\*\//,
+  JSFlag  : /^\/\/.*enable javascript.*|^\/\*.*enable javascript.*\*\//,
+
+  codeElementDepth: 2,
+  insertPosition  : 'afterend',
+};
 
 module.exports = function liveDemo (options) {
-  options.HTMLSelector = options.HTMLSelector || 'code.language-xml,code.language-html,code.language-markup,code.xml,code.html';
-  options.CSSSelector  = options.CSSSelector  || 'code.language-css,code.css';
-  options.JSSelector   = options.JSSelector   || 'code.language-javascript,code.javascript';
+  options.HTMLSelector = options.HTMLSelector || defaults.HTMLSelector;
+  options.CSSSelector  = options.CSSSelector  || defaults.CSSSelector;
+  options.JSSelector   = options.JSSelector   || defaults.JSSelector;
 
-  options.HTMLFlag = options.HTMLFlag || /^<!--.*enable javascript.*-->/;
-  options.CSSFlag  = options.CSSFlag  || /^\/\*.*enable javascript.*\*\//;
-  options.JSFlag   = options.JSFlag   || /^\/\/.*enable javascript.*|^\/\*.*enable javascript.*\*\//;
+  options.HTMLFlag = options.HTMLFlag || defaults.HTMLFlag;
+  options.CSSFlag  = options.CSSFlag  || defaults.CSSFlag;
+  options.JSFlag   = options.JSFlag   || defaults.JSFlag;
 
-  options.codeElementDepth = typeof options.codeElementDepth === 'number'? options.codeElementDepth: 2;
-  options.insertPosition   = options.insertPosition || 'afterend';
+  options.codeElementDepth = typeof options.codeElementDepth === 'number'? options.codeElementDepth: defaults.codeElementDepth;
+  options.insertPosition   = options.insertPosition || defaults.insertPosition;
 
-  const xmlCodeBlocks = $(options.HTMLSelector),
+  var xmlCodeBlocks = $(options.HTMLSelector),
     cssCodeBlocks = $(options.CSSSelector),
     jsCodeBlocks  = $(options.JSSelector);
 
   // get <code> elements with comments that match the XML/HTML flag
-  xmlCodeBlocks = xmlCodeBlocks.filter(element => {
+  xmlCodeBlocks = xmlCodeBlocks.filter(function(element) {
     return options.HTMLFlag.test(element.firstChild.textContent);
   });
 
-  xmlCodeBlocks.forEach(element => {
+  xmlCodeBlocks.forEach(function (element) {
     // remove flag comment
     element.removeChild(element.firstChild);
 
@@ -33,18 +46,18 @@ module.exports = function liveDemo (options) {
     // insert code as HTML in a div.livedemo element
     // by default after the <code> element's parent's parent
     // e.g. code -> pre -> div.highlight *div.live-demo*
-    getInsertElement(element).insertAdjacentHTML(
+    getInsertElement(element, options.codeElementDepth).insertAdjacentHTML(
       options.insertPosition,
-      `<div class="live-demo">${element.textContent}</div>`);
+      '<div class="live-demo">' + element.textContent + '</div>');
   });
 
 
   // get <code> elements with comments that match the CSS flag
-  cssCodeBlocks = cssCodeBlocks.filter(element => {
+  cssCodeBlocks = cssCodeBlocks.filter(function(element) {
     return options.CSSFlag.test(element.firstChild.textContent);
   });
 
-  cssCodeBlocks.forEach(element => {
+  cssCodeBlocks.forEach(function(element) {
     // remove flag comment
     element.removeChild(element.firstChild);
 
@@ -56,16 +69,16 @@ module.exports = function liveDemo (options) {
     // insert code as a <style> in <head>
     document.head.insertAdjacentHTML(
       'beforeend',
-      `<style>${element.textContent}</style>`);
+      '<style>' + element.textContent + '</style>');
   });
 
 
   // get <code> elements with comments that match the javascript flag
-  jsCodeBlocks = jsCodeBlocks.filter(element => {
+  jsCodeBlocks = jsCodeBlocks.filter(function(element) {
     return options.JSFlag.test(element.firstChild.textContent);
   });
 
-  jsCodeBlocks.forEach(element => {
+  jsCodeBlocks.forEach(function (element) {
     // remove flag comment
     element.removeChild(element.firstChild);
 
@@ -79,15 +92,17 @@ module.exports = function liveDemo (options) {
       new Function(element.textContent)();
     } catch (error) {
       console.error('livedemo failed to execute a javascript code block');
-      console.log({ element, error });
+      console.log({ element: element, error: error });
     }
   });
-}
+};
+
+module.exports.options = defaults;
 
 // returns the element relative to which the HTML will be inserted
 // climbs up DOM tree from codeElement `codeElementDepth` times
-function getInsertElement (codeElement) {
-  for (const i = 0; i < options.codeElementDepth; i++) {
+function getInsertElement (codeElement, codeElementDepth) {
+  for (var i = 0; i < codeElementDepth; i++) {
     codeElement = codeElement.parentNode;
   }
   return codeElement;
